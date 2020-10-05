996f4f1733a0fbee5314877b9e23adb27a7fd3b1
C-Domenech <57828876+C-Domenech@users.noreply.github.com>
2020-10-05 17:02:35 +0200

    comments updated

--- Txt Editor/TextEditorUI.java
+++ Txt Editor/TextEditorUI.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package textEditorUI;
 
 import java.io.BufferedReader;
@@ -20,7 +15,7 @@ import javax.swing.JFileChooser;
 
 /**
  *
- * @author C-Domenech
+ * @author Cristina Domenech <github.com/C-Domenech>
  */
 public class TextEditorUI extends javax.swing.JFrame {
 
